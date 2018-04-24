a3ab172cfc1b4c3de61e9565d49b62bbaee65b5f
Admin User <salahuddeen.ahmed@my.uwi.edu>
2018-04-23 20:32:00 -0400

    Templates build

--- info340MatrixOfYou/MatrixOfYouApp/urls.py
+++ info340MatrixOfYou/MatrixOfYouApp/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from . import views
+
 
 urlpatterns = [
-
+    url(r'^base', views.template, name='homepage'),
+    url(r'^', views.homepage, name='__homepage'),
 ]
--- info340MatrixOfYou/MatrixOfYouApp/views.py
+++ info340MatrixOfYou/MatrixOfYouApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def homepage(request):
+    return HttpResponse("Homepage")
+
+def template(request):
+    return render(request, 'base.html', context = {})
